1b330f5dbe356b196ac52ecdef99ec17114679ad
dyiar <austin.hl.blake@gmail.com>
2019-02-04 16:02:57 -0700

    trying again with adding engines in json file.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { server } = require('./api/server');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`\n=== Server up on port ${port}\n`)
